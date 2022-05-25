af5e8d4e881b2d788c69de7460bf52082acc9c3f
nawawan <mizutake0428@yahoo.co.jp>
2022-05-25 10:57:14 +0900

    環境構築

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    static:{
+        directory: path.resolve(__dirname, 'public'),
+        publicPath: "/"
+    }
+  }
+}
