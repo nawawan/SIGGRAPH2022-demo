a34a075e613902b8d5bb75ab0aec767f1edb61be
nawawan <mizutake0428@yahoo.co.jp>
2022-05-25 10:58:27 +0900

    まだ使わないけど雛形をコミット

---
+++ src/server.js
@@ -0,0 +1,38 @@
+
+var express = require('express');
+
+// ポートをopen
+var app = express();
+var server = app.listen(3000);
+// publicフォルダを使っていくよってこと？
+app.use(express.static('public'));
+
+console.log("My socket server is running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+// OSCでつながった時に呼ばれる関数
+function newConnection(socket) {
+    console.log('new connection: ' + socket.id)
+
+    // padsやmatchという名前のdataを受信
+    // 受信したら第二引数の関数がcallbackする
+    /*
+    socket.on('pads', controllerMsg);
+    socket.on('match', matchMsg);
+
+    // padsを受信した時に発動
+    function controllerMsg(data) {
+        socket.broadcast.emit('pads', data);
+    }
+
+    // matchを受信した時に発動
+    function matchMsg(data) {
+        socket.broadcast.emit('match', data);
+    }
+*/
+}
