4ed676de3c4b84ee16470ed338406fbfd73edd74
nawawan <mizutake0428@yahoo.co.jp>
2022-05-28 04:32:40 +0900

    動画をふよ

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,24 @@
 export const sketch = (p) => {
+  let vid;
   p.setup = () => {
+    vid = p.createVideo("asset/card.mov", vidLoad);
+    vid.size(p.windowWidth / 3, p.windowHeight);
+    vid.position(0, 0);
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.noStroke();
+    p.background(0);
   }
   p.draw = () => {
-    p.background('#0f2350');
-    p.fill(255);
+    p.background(0);
     if(p.keyIsPressed){
       p.fill('green');
     }else{
       p.fill('white');
     }
     p.ellipse(p.mouseX, p.mouseY, 50, 50);
+  }
+  const vidLoad = () => {
+    vid.loop();
+    vid.volume(0);
   }
 };
