d8d9f872ec1dc66dae449ad86fc2043740e3a83b
nawawan <mizutake0428@yahoo.co.jp>
2022-05-28 05:12:59 +0900

    ビデオの管理を配列で

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,15 @@
 export const sketch = (p) => {
-  let vid;
+  let vid = [];
+  let duration, duration2;
   p.setup = () => {
-    vid = p.createVideo("asset/card.mov", vidLoad);
-    vid.size(p.windowWidth / 3, p.windowHeight);
-    vid.position(0, 0);
+    let v = p.createVideo("asset/card.mov");
+    vid.push(v);
+    vid[0].size(p.windowWidth / 6, p.windowHeight / 3);
+    vid[0].position(p.windowWidth / 3, 0);
+    v = p.createVideo("asset/card.mov");
+    vid.push(v);
+    vid[1].size(p.windowWidth / 6, p.windowHeight / 3);
+    vid[1].position(p.windowWidth / 2, 0);
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.noStroke();
     p.background(0);
@@ -16,9 +22,5 @@ export const sketch = (p) => {
       p.fill('white');
     }
     p.ellipse(p.mouseX, p.mouseY, 50, 50);
-  }
-  const vidLoad = () => {
-    vid.loop();
-    vid.volume(0);
   }
 };
